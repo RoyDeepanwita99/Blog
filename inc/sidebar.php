4ad3c1ea2197b4b64d1a543c13593176a39a6f23
RoyDeepanwita99 <101567200+RoyDeepanwita99@users.noreply.github.com>
2023-09-30 21:20:58 +0600

    sidebar.php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -2,20 +2,32 @@
 			<div class="samesidebar clear">
 				<h2>Categories</h2>
 					<ul>
-						<li><a href="#">Category One</a></li>
-						<li><a href="#">Category Two</a></li>
-						<li><a href="#">Category Three</a></li>
-						<li><a href="#">Category Four</a></li>
-						<li><a href="#">Category Five</a></li>						
+						<li><a href="#">Java</a></li>
+						<li><a href="#">PHP</a></li>
+						<li><a href="#">CSS</a></li>	
+						<li><a href="#">WEB Engineering</a></li>
+						<li><a href="#">Machine Learning</a></li>
+						<li><a href="#">Programing</a></li>
+						<li><a href="#">Deep Learning</a></li>
+						<li><a href="#">Neural  Network</a></li>
+						<li><a href="#">Travel</a></li>
+						<li><a href="#">Life Style</a></li>
+						<li><a href="#">blogging</a></li>
 					</ul>
 			</div>
 			
 			<div class="samesidebar clear">
 				<h2>Latest articles</h2>
 					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
+						<h3><a href="#">Dynamic Field Programmable Logic-Driven Soft Exosuit</a></h3>
 						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+						<p>The next generation of etextiles foresees an era of smart wearable garments where embedded seamless intelligence provides the ability to sense, process and perform. Core to this vision is embedded textile functionality enabling dynamic configuration. </p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Databse Managment System</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Database Management System is a software or technology used to manage data from a database. Some popular databases are MySQL, Oracle, MongoDB, etc. DBMS provides many operations e.g. creating a database, Storing in the database, updating an existing database, delete from the database. DBMS is a system that enables you to store, modify and retrieve data in an organized way. It also provides security to the database..</p>	
 					</div>
 					
 					<div class="popular clear">
@@ -25,15 +37,11 @@
 					</div>
 					
 					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
+						<h3><a href="#">Entity relationship model.</a></h3>
 						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+						<p>The Entity Relational Model is a model for identifying entities to be represented in the database and representation of how those entities are related. The ER data model specifies enterprise schema that represents the overall logical structure of a database graphically. 
+
+The Entity Relationship Diagram explains the relationship among the entities present in the database. ER models are used to model real-world objects like a person, a car, or a company and the relation between these real-world objects. In short, the ER Diagram is the structural format of the database. </p>	
 					</div>
 	
 			</div>
